e0b9d5b7e20b3786130364e41770acd7a1cbbe72
Khorlane <khorlane@yahoo.com>
2018-10-31 20:37:39 -0400

    Stage 3 tweaking
    More Cosmetic changes

--- BrokenThorn/Stage3.asm
+++ BrokenThorn/Stage3.asm
@@ -14,25 +14,16 @@
     JMP   Stage3                        ; jump to entry point
 
 ;--------------------------------------------------------------------------------------------------
-; 32 bit Video Routines
+; Video Routines
 ;--------------------------------------------------------------------------------------------------
-[bits 32]
-VIDMEM      EQU 0B8000h                 ; video memory
-COLS        EQU 80                      ; width and height of screen
-LINES       EQU 25
-CHAR_ATTRIB EQU 63                      ; character attribute (White text on light blue background)
-
-_CurX       DB  0                       ; current x/y location
-_CurY       DB  0
 
 ;----------------------------
 ; Print a character to screen
 ; BL = Character to print
 ;----------------------------
-[bits 32]
 PutCh:
     PUSHA                               ; save registers
-    MOV   EDI,VIDMEM                    ; get pointer to video memory
+    MOV   EDI,VidMem                    ; get pointer to video memory
 
     ;---------------------
     ; Get current position
@@ -40,33 +31,33 @@ PutCh:
     XOR   EAX,EAX                       ; clear eax
 
     ;----------------------------------------------------
-    ; Remember: currentPos = x + y * COLS!
-    ; x and y are in _CurX and _CurY.
+    ; Remember: currentPos = x + y * Cols!
+    ; x and y are in CurX and CurY.
     ; Because there are two bytes per character,
-    ; COLS=number of characters in a line.
+    ; Cols=number of characters in a line.
     ; We have to multiply this by 2 to get number
     ; of bytes per line. This is the screen width,
-    ; so multiply screen with * _CurY to get current line
+    ; so multiply screen with * CurY to get current line
     ;----------------------------------------------------
-    MOV   ECX,COLS*2                    ; Mode 7 has 2 bytes per char, so its COLS*2 bytes per line
-    MOV   AL,BYTE [_CurY]               ; get y pos
-    MUL   ECX                           ; multiply y*COLS
+    MOV   ECX,Cols*2                    ; Mode 7 has 2 bytes per char, so its Cols*2 bytes per line
+    MOV   AL,BYTE [CurY]                ; get y pos
+    MUL   ECX                           ; multiply y*Cols
     PUSH  EAX                           ; save eax--the multiplication
 
     ;-------------------------------------
     ; Now y * screen width is in eax.
-    ; Now, just add _CurX. But, again
-    ; remember that _CurX is relative
+    ; Now, just add CurX. But, again
+    ; remember that CurX is relative
     ; to the current character count,
     ; not byte count. Because there are
     ; two bytes per character, we
-    ; have to multiply _CurX by 2 first,
+    ; have to multiply CurX by 2 first,
     ; then add it to our screen width * y.
     ;-------------------------------------
-    MOV   AL,BYTE [_CurX]               ; multiply _CurX by 2 because it is 2 bytes per char
+    MOV   AL,BYTE [CurX]                ; multiply CurX by 2 because it is 2 bytes per char
     MOV   CL,2
     MUL   CL
-    POP   ECX                           ; pop y*COLS result
+    POP   ECX                           ; pop y*Cols result
     ADD   EAX,ECX
 
     ;------------------------------------
@@ -82,20 +73,20 @@ PutCh:
     ; Watch for new line
     ;-------------------
     CMP   BL,0Ah                        ; is it a newline character?
-    JE    PutCh1                          ; yep--go to next row
+    JE    PutCh1                        ; yep--go to next row
 
     ;------------------
     ; Print a character
     ;------------------
     MOV   DL,BL                         ; Get character
-    MOV   DH,CHAR_ATTRIB                ; the character attribute
+    MOV   DH,ChAttrib                   ; the character attribute
     MOV   WORD [EDI],DX                 ; write to video display
 
     ;---------------------
     ; Update next position
     ;---------------------
-    INC   BYTE [_CurX]                  ; go to next character
-  ; CMP   BYTE [_CurX],COLS             ; are we at the end of the line?
+    INC   BYTE [CurX]                   ; go to next character
+  ; CMP   BYTE [CurX],Cols              ; are we at the end of the line?
   ; JE    PutCh1                        ; yep-go to next row
     JMP   PutCh2                        ; nope, bail out
 
@@ -103,8 +94,8 @@ PutCh:
     ; Go to next row
     ;---------------
   PutCh1:
-    MOV BYTE [_CurX],0                  ; go back to col 0
-    INC BYTE [_CurY]                    ; go to next row
+    MOV BYTE [CurX],0                   ; go back to col 0
+    INC BYTE [CurY]                     ; go to next row
 
     ;---------------------------
     ; Restore registers & return
@@ -117,7 +108,6 @@ PutCh:
 ; Print a null terminated string
 ; EBX = address of string to print
 ;---------------------------------
-[bits 32]
 PutStr:
     ; Save registers
     PUSHA                               ; save registers
@@ -131,7 +121,7 @@ PutStr1:
     JE    PutStr2                       ; yep-bail out
 
     ; Print the character
-    CALL  PutCh                       ; Nope-print it out
+    CALL  PutCh                         ; Nope-print it out
 
     ; Go to next character
     INC   EDI                           ; go to next character
@@ -142,8 +132,8 @@ PutStr2:
     ; Its more efficiant to update the cursor after displaying
     ; the complete string because direct VGA is slow
 
-    MOV   BH,BYTE [_CurY]               ; get current position
-    MOV   BL,BYTE [_CurX]
+    MOV   BH,BYTE [CurY]                ; get current position
+    MOV   BL,BYTE [CurX]
     CALL  MovCursor                     ; update cursor
 
     POPA                                ; restore registers, and return
@@ -154,18 +144,17 @@ PutStr2:
 ; bh = Y pos
 ; bl = x pos
 ;-----------------------
-[bits 32]
 MovCursor:
     PUSHA                               ; save registers (aren't you getting tired of this comment?)
 
     ; Get current position
-    ; Here, _CurX and _CurY are relitave to the current position on screen, not in memory.
+    ; Here, CurX and CurY are relitave to the current position on screen, not in memory.
     ; That is, we don't need to worry about the byte alignment we do when displaying characters,
-    ; so just follow the forumla: location = _CurX + _CurY * COLS
+    ; so just follow the forumla: location = CurX + CurY * Cols
     XOR   EAX,EAX
-    MOV   ECX,COLS
+    MOV   ECX,Cols
     MOV   AL,BH                         ; get y pos
-    MUL   ECX                           ; multiply y*COLS
+    MUL   ECX                           ; multiply y*Cols
     ADD   AL,BL                         ; Now add x
     MOV   EBX,EAX
 
@@ -195,26 +184,22 @@ MovCursor:
 ;-------------
 ; Clear Screen
 ;-------------
-[bits 32]
-ClrScr32:
+ClrSrc:
     PUSHA
     CLD
-    MOV   EDI,VIDMEM
+    MOV   EDI,VidMem
     MOV   CX,2000
-    MOV   AH,CHAR_ATTRIB
+    MOV   AH,ChAttrib
     MOV   AL,' '
     REP   STOSW
-    MOV   BYTE [_CurX],0
-    MOV   BYTE [_CurY],0
+    MOV   BYTE [CurX],0
+    MOV   BYTE [CurY],0
     POPA
     RET
 
 ;--------------------------------------------------------------------------------------------------
 ; Stage3 - Our Kernel!
 ;--------------------------------------------------------------------------------------------------
-Msg DB  0x0A, 0x0A, "                     ------    My Os v2     -----"
-    DB  0x0A, 0x0A, "                     ------  32 Bit Kernel  -----", 0x0A, 0
-
 Stage3:
     ;--------------
     ; Set registers
@@ -228,12 +213,25 @@ Stage3:
     ;-------------------------------
     ; Clear screen and print success
     ;-------------------------------
-    CALL  ClrScr32               
+    CALL  ClrSrc
     MOV   EBX,Msg
-    CALL  PutStr  
+    CALL  PutStr
 
     ;---------------
     ; Stop execution
     ;---------------
     CLI
     HLT
+
+;--------------------------------------------------------------------------------------------------
+; Working Storage
+;--------------------------------------------------------------------------------------------------
+Msg         DB  0x0A, 0x0A, "                     ------    My Os v1     -----"
+            DB  0x0A, 0x0A, "                     ------  32 Bit Kernel  -----", 0x0A, 0
+
+ChAttrib    EQU 63                      ; character attribute (White text on light blue background)
+Cols        EQU 80                      ; width and height of screen
+CurX        DB  0                        ; current x/y location
+CurY        DB  0
+Lines       EQU 25                      ; not used
+VidMem      EQU 0B8000h                 ; video memory
